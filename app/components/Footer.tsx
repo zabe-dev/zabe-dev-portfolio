03eeb1240a5bb7c3a3412bc33d128328d96409c2
zabe-dev <jay@zabe.dev>
2026-08-26 15:47:35 +0800

    Make footer year automatic

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,14 @@
+'use client';
+
 import { SocialLinks } from './SocialLinks';
 
 export function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer>
       <a className="footer-home" href="/" aria-label="Go to homepage">
-        JAY BEZA · 2026
+        JAY BEZA · <span suppressHydrationWarning>{currentYear}</span>
       </a>
       <div>
         <SocialLinks />
